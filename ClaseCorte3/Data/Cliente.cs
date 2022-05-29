17aeb89a0acfc271459b213515d3a2b04ce63c50
CamiloNaOr <99045832+CamiloNaOr@users.noreply.github.com>
2022-05-28 19:19:41 -0500

    Add files via upload

---
+++ ClaseCorte3/Data/Cliente.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace ClaseCorte3.Data
+{
+    public class Cliente
+    {
+        [Key]
+        public string NombreCliente { get; set; }
+        public string NombreEmpresa { get; set; }
+        public int Compras { get; set; }
+        public int Credito { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ ClaseCorte3/Data/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace ClaseCorte3.Data
+{
+    public class Producto
+    {
+        [Key]
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public int cantidad { get; set; }
+        public int precio { get; set; }
+        public string descripcion { get; set; }
+    }
+}
